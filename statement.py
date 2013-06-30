385af0d6aa40a8f8c75b6373bc8fbf72a3b3ffba
Albert Cervera i Areny <albert@nan-tic.com>
2013-06-30 18:50:47 +0200

    Initial commit.

---
+++ statement.py
@@ -0,0 +1,239 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+
+from decimal import Decimal
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.pyson import Eval, Not, Equal
+from trytond.pool import Pool, PoolMeta
+
+__metaclass__ = PoolMeta
+__all__ = ['StatementLine', 'StatementMoveLine']
+
+POSTED_STATES = {
+    'readonly': Not(Equal(Eval('state'), 'confirmed'))
+    }
+_ZERO = Decimal("0.0")
+
+
+class StatementLine:
+    __name__ = 'account.bank.statement.line'
+
+    lines = fields.One2Many('account.bank.statement.move.line',
+        'line', 'Transactions', states=POSTED_STATES,)
+
+    @classmethod
+    def __setup__(cls):
+        super(StatementLine, cls).__setup__()
+        if 'lines' not in cls.moves_amount.on_change_with:
+            cls.moves_amount.on_change_with.append('lines')
+
+    @classmethod
+    def post(cls, statement_lines):
+        for st_line in statement_lines:
+            for line in st_line.lines:
+                line.create_move()
+        super(StatementLine, cls).post(statement_lines)
+
+    def on_change_with_moves_amount(self):
+        res = super(StatementLine, self).on_change_with_moves_amount()
+        if getattr(self, 'state', None) == 'posted':
+            return res
+        return res + sum(l.amount or Decimal('0.0') for l in self.lines)
+
+    @classmethod
+    def cancel(cls, statement_lines):
+        super(StatementLine, cls).cancel(statement_lines)
+        for st_line in statement_lines:
+            st_line.reset_account_move()
+
+    def reset_account_move(self):
+        pool = Pool()
+        Move = pool.get('account.move')
+        delete_moves = [x.move for x in self.lines if x.move]
+        Move.draft(delete_moves)
+        Move.delete(delete_moves)
+
+
+class StatementMoveLine(ModelSQL, ModelView):
+    'Statement Move Line'
+    __name__ = 'account.bank.statement.move.line'
+
+    line = fields.Many2One('account.bank.statement.line', 'Line',
+        required=True, ondelete='CASCADE')
+    date = fields.Date('Date', required=True)
+    amount = fields.Numeric('Amount', required=True,
+        digits=(16, Eval('_parent_statement', {}).get('currency_digits', 2)),
+        on_change=['amount', 'party', 'account', 'invoice',
+            '_parent_line.journal'])
+    party = fields.Many2One('party.party', 'Party',
+            on_change=['amount', 'party', 'invoice'])
+    account = fields.Many2One('account.account', 'Account', required=True,
+        on_change=['account', 'invoice'], domain=[
+            ('company', '=', Eval('_parent_line', {}).get('company', 0)),
+            ('kind', '!=', 'view'),
+            ])
+    description = fields.Char('Description')
+    move = fields.Many2One('account.move', 'Account Move', readonly=True)
+
+    @classmethod
+    def __setup__(cls):
+        super(StatementMoveLine, cls).__setup__()
+        cls._sql_constraints += [(
+                'check_bank_move_amount', 'CHECK(amount != 0)',
+                'Amount should be a positive or negative value.'),
+        ]
+        cls._error_messages.update({
+                'debit_credit_account_statement_journal': ('Please provide '
+                    'debit and credit account on statement journal "%s".'),
+                'same_debit_credit_account': ('Account "%(account)s" in '
+                    'statement line "%(line)s" is the same as the one '
+                    'configured as credit or debit on journal "%(journal)s".'),
+                })
+
+    @staticmethod
+    def default_amount():
+        return Decimal(0)
+
+    def on_change_party(self):
+        res = {}
+        if self.party and self.amount:
+                if self.amount > Decimal("0.0"):
+                    account = self.account or self.party.account_receivable
+                else:
+                    account = self.account or self.party.account_payable
+                res['account'] = account.id
+                res['account.rec_name'] = account.rec_name
+
+        return res
+
+    def on_change_amount(self):
+        res = {}
+        if self.party and not self.account and self.amount:
+            if self.amount > Decimal("0.0"):
+                account = self.party.account_receivable
+            else:
+                account = self.party.account_payable
+            res['account'] = account.id
+            res['account.rec_name'] = account.rec_name
+        return res
+
+    def on_change_account(self):
+        res = {}
+        if self.invoice:
+            if self.account:
+                if self.invoice.account != self.account:
+                    res['invoice'] = None
+            else:
+                res['invoice'] = None
+        return res
+
+    def get_rec_name(self, name):
+        return self.line.rec_name
+
+    @classmethod
+    def copy(cls, lines, default=None):
+        if default is None:
+            default = {}
+        default = default.copy()
+        default.setdefault('move', None)
+        default.setdefault('invoice', None)
+        return super(StatementMoveLine, cls).copy(lines, default=default)
+
+    def create_move(self):
+        '''
+        Create move for the statement line and return move if created.
+        '''
+        pool = Pool()
+        Move = pool.get('account.move')
+        Period = pool.get('account.period')
+
+        if self.move:
+            return
+
+        period_id = Period.find(self.line.company.id, date=self.date)
+
+        move_lines = self._get_move_lines()
+        move = Move(
+            period=period_id,
+            journal=self.line.journal.journal,
+            date=self.date,
+            lines=move_lines,
+            )
+        move.save()
+        Move.post([move])
+
+        journal = self.line.journal.journal
+        accounts = [journal.credit_account, journal.debit_account]
+
+        st_move_line, = [x for x in move.lines if x.account in accounts]
+        bank_line, = st_move_line.bank_lines
+        bank_line.bank_statement_line = self.line
+        bank_line.save()
+
+        self.move = move
+        self.save()
+        return move
+
+    @classmethod
+    def post_move(cls, lines):
+        Move = Pool().get('account.move')
+        Move.post([l.move for l in lines if l.move])
+
+    @classmethod
+    def delete_move(cls, lines):
+        Move = Pool().get('account.move')
+        Move.delete([l.move for l in lines if l.move])
+
+    def _get_move_lines(self):
+        '''
+        Return the move lines for the statement line
+        '''
+        pool = Pool()
+        MoveLine = pool.get('account.move.line')
+        Currency = Pool().get('currency.currency')
+        amount = Currency.compute(self.line.journal.currency, self.amount,
+            self.line.company.currency)
+        if self.line.journal.currency != self.line.company.currency:
+            second_currency = self.line.journal.currency.id
+            amount_second_currency = abs(self.amount)
+        else:
+            amount_second_currency = None
+            second_currency = None
+
+        move_lines = []
+        move_lines.append(MoveLine(
+                description=self.description,
+                debit=amount < _ZERO and -amount or _ZERO,
+                credit=amount >= _ZERO and amount or _ZERO,
+                account=self.account,
+                party=self.party,
+                second_currency=second_currency,
+                amount_second_currency=amount_second_currency,
+                ))
+
+        journal = self.line.journal.journal
+        if self.amount >= _ZERO:
+            account = journal.credit_account
+        else:
+            account = journal.debit_account
+        if not account:
+            self.raise_user_error('debit_credit_account_statement_journal',
+                journal.rec_name)
+        if self.account == account:
+            self.raise_user_error('same_debit_credit_account', {
+                    'account': self.account.rec_name,
+                    'line': self.account,
+                    'journal': self.journal,
+                    })
+
+        bank_move = MoveLine(
+            description=self.description,
+            debit=amount >= _ZERO and amount or _ZERO,
+            credit=amount < _ZERO and -amount or _ZERO,
+            account=account,
+            party=self.party,
+            second_currency=second_currency,
+            amount_second_currency=amount_second_currency,
+            )
+        move_lines.append(bank_move)
+        return move_lines
---
+++ tests/scenario_bank_statement.rst
@@ -0,0 +1,194 @@
+===============================
+Account Bank Statement Scenario
+===============================
+
+Imports::
+
+    >>> import datetime
+    >>> from dateutil.relativedelta import relativedelta
+    >>> from decimal import Decimal
+    >>> from operator import attrgetter
+    >>> from proteus import config, Model, Wizard
+    >>> today = datetime.date.today()
+    >>> now = datetime.datetime.now()
+
+Create database::
+
+    >>> config = config.set_trytond()
+    >>> config.pool.test = True
+
+Install account_bank_statement::
+
+    >>> Module = Model.get('ir.module.module')
+    >>> account_bank_module, = Module.find(
+    ...     [('name', '=', 'account_bank_statement_account')])
+    >>> Module.install([account_bank_module.id], config.context)
+    >>> Wizard('ir.module.module.install_upgrade').execute('upgrade')
+
+Create company::
+
+    >>> Currency = Model.get('currency.currency')
+    >>> CurrencyRate = Model.get('currency.currency.rate')
+    >>> currencies = Currency.find([('code', '=', 'USD')])
+    >>> if not currencies:
+    ...     currency = Currency(name='US Dollar', symbol=u'$', code='USD',
+    ...         rounding=Decimal('0.01'), mon_grouping='[]',
+    ...         mon_decimal_point='.')
+    ...     currency.save()
+    ...     CurrencyRate(date=today + relativedelta(month=1, day=1),
+    ...         rate=Decimal('1.0'), currency=currency).save()
+    ... else:
+    ...     currency, = currencies
+    >>> Company = Model.get('company.company')
+    >>> Party = Model.get('party.party')
+    >>> company_config = Wizard('company.company.config')
+    >>> company_config.execute('company')
+    >>> company = company_config.form
+    >>> party = Party(name='Dunder Mifflin')
+    >>> party.save()
+    >>> company.party = party
+    >>> company.currency = currency
+    >>> company_config.execute('add')
+    >>> company, = Company.find([])
+
+Reload the context::
+
+    >>> User = Model.get('res.user')
+    >>> config._context = User.get_preferences(True, config.context)
+
+Create fiscal year::
+
+    >>> FiscalYear = Model.get('account.fiscalyear')
+    >>> Sequence = Model.get('ir.sequence')
+    >>> fiscalyear = FiscalYear(name=str(today.year))
+    >>> fiscalyear.start_date = today + relativedelta(month=1, day=1)
+    >>> fiscalyear.end_date = today + relativedelta(month=12, day=31)
+    >>> fiscalyear.company = company
+    >>> post_move_seq = Sequence(name=str(today.year), code='account.move',
+    ...     company=company)
+    >>> post_move_seq.save()
+    >>> fiscalyear.post_move_sequence = post_move_seq
+    >>> fiscalyear.save()
+    >>> FiscalYear.create_period([fiscalyear.id], config.context)
+
+Create chart of accounts::
+
+    >>> AccountTemplate = Model.get('account.account.template')
+    >>> Account = Model.get('account.account')
+    >>> account_template, = AccountTemplate.find([('parent', '=', False)])
+    >>> create_chart = Wizard('account.create_chart')
+    >>> create_chart.execute('account')
+    >>> create_chart.form.account_template = account_template
+    >>> create_chart.form.company = company
+    >>> create_chart.execute('create_account')
+    >>> receivable, = Account.find([
+    ...         ('kind', '=', 'receivable'),
+    ...         ('company', '=', company.id),
+    ...         ])
+    >>> payable, = Account.find([
+    ...         ('kind', '=', 'payable'),
+    ...         ('company', '=', company.id),
+    ...         ])
+    >>> revenue, = Account.find([
+    ...         ('kind', '=', 'revenue'),
+    ...         ('company', '=', company.id),
+    ...         ])
+    >>> expense, = Account.find([
+    ...         ('kind', '=', 'expense'),
+    ...         ('company', '=', company.id),
+    ...         ])
+    >>> account_tax, = Account.find([
+    ...         ('kind', '=', 'other'),
+    ...         ('company', '=', company.id),
+    ...         ('name', '=', 'Main Tax'),
+    ...         ])
+    >>> cash, = Account.find([
+    ...         ('kind', '=', 'other'),
+    ...         ('company', '=', company.id),
+    ...         ('name', '=', 'Main Cash'),
+    ...         ])
+    >>> cash.bank_reconcile = True
+    >>> cash.reconcile = True
+    >>> cash.save()
+    >>> create_chart.form.account_receivable = receivable
+    >>> create_chart.form.account_payable = payable
+    >>> create_chart.execute('create_properties')
+
+Create party::
+
+    >>> Party = Model.get('party.party')
+    >>> party = Party(name='Party')
+    >>> party.save()
+
+Create journals::
+
+    >>> sequence = Sequence(name='Bank', code='account.journal',
+    ...     company=company)
+    >>> sequence.save()
+    >>> AccountJournal = Model.get('account.journal')
+    >>> account_journal = AccountJournal(name='Statement',
+    ...     type='bank_statement',
+    ...     credit_account=cash,
+    ...     debit_account=cash,
+    ...     sequence=sequence)
+    >>> account_journal.save()
+    >>> StatementJournal = Model.get('account.bank.statement.journal')
+    >>> statement_journal = StatementJournal(name='Test',
+    ...     journal=account_journal)
+    >>> statement_journal.save()
+
+Create move::
+
+    >>> period = fiscalyear.periods[0]
+    >>> Move = Model.get('account.move')
+    >>> move = Move()
+    >>> move.period = period
+    >>> move.journal = account_journal
+    >>> move.date = period.start_date
+    >>> line = move.lines.new()
+    >>> line.account = cash
+    >>> line.debit = Decimal('80.0')
+    >>> line.party = party
+    >>> line2 = move.lines.new()
+    >>> line2.account = receivable
+    >>> line2.credit = Decimal('80.0')
+    >>> line2.party = party
+    >>> move.save()
+    >>> move.reload()
+    >>> Move.post([move.id], config.context)
+    >>> move.reload()
+    >>> move.state
+    u'posted'
+
+Create bank statement::
+
+    >>> BankStatement = Model.get('account.bank.statement')
+    >>> statement = BankStatement(journal=statement_journal, date=now)
+
+Create bank statement lines::
+
+    >>> StatementLine = Model.get('account.bank.statement.line')
+    >>> statement_line = StatementLine()
+    >>> statement.lines.append(statement_line)
+    >>> statement_line.date = now
+    >>> statement_line.description = 'Statement Line'
+    >>> statement_line.amount = Decimal('80.0')
+    >>> statement_line.account = revenue
+    >>> statement.save()
+    >>> statement.reload()
+    >>> BankStatement.confirm([statement.id], config.context)
+    >>> statement.reload()
+    >>> statement.state
+    u'confirmed'
+    >>> statement_line = StatementLine(1)
+    >>> StatementMoveLine = Model.get('account.bank.statement.move.line')
+    >>> st_move_line = StatementMoveLine()
+    >>> st_move_line.amount = Decimal('80.0')
+    >>> st_move_line.line = statement_line
+    >>> st_move_line.account = revenue
+    >>> st_move_line.date = today
+    >>> st_move_line.save()
+    >>> st_move_line.reload()
+    >>> StatementLine.post([statement_line.id], config.context)
+    >>> statement_line.company_amount == Decimal('80.0')
+    True
