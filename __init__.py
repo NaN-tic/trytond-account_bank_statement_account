cb7b8cdde7350a92035463c633b09fc29e810f35
Albert Cervera i Areny <albert@nan-tic.com>
2016-05-29 23:12:19 +0200

    Do not allow the user to move an invoice back to draft state when posted, if it is linked in a bank statement line. (grafted from 066fe2b18a3d3545a411e5c9bbd5711402335d09)

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@
 #copyright notices and license terms.
 
 from trytond.pool import Pool
+from .invoice import *
 from .statement import *
 
 
 def register():
     Pool.register(
+        Invoice,
         StatementLine,
         StatementMoveLine,
         module='account_bank_statement_account', type_='model')
