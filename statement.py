ff5657b5dc4899297eaa9881e5c4105be848f4e6
Bernat Brunet <bernat@nan-tic.com>
2023-05-14 10:48:50 +0200

    Add the relation of the bank statement and bank statement line to the account move and account move line create.
    Tasks: #158782

--- statement.py
+++ statement.py
@@ -11,8 +11,6 @@ from trytond.exceptions import UserError
 from sql import Null
 
 
-__all__ = ['StatementLine', 'StatementMoveLine']
-
 POSTED_STATES = {
     'readonly': Not(Equal(Eval('state'), 'confirmed'))
     }
@@ -272,6 +270,7 @@ class StatementMoveLine(ModelSQL, ModelView):
             date=self.date,
             lines=move_lines,
             description=self.description,
+            origin=self.line.statement,
             )
 
     def _get_move_lines(self):
@@ -311,6 +310,7 @@ class StatementMoveLine(ModelSQL, ModelView):
                 party=self.party if self.account.party_required else None,
                 second_currency=second_currency,
                 amount_second_currency=amount_second_currency,
+                origin=self.line,
                 ))
 
         journal = self.line.journal
@@ -342,6 +342,8 @@ class StatementMoveLine(ModelSQL, ModelView):
                 if account.party_required else None),
             second_currency=second_currency,
             amount_second_currency=amount_second_currency,
+            origin=self.line,
+            move_origin=self.line.statement,
             )
         move_lines.append(bank_move)
         return move_lines
